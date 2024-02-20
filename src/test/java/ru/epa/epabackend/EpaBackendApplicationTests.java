52bd1ff7c748c54518f1227e05f4eb6f3f7fb37b
Mikhail Bezuglov <bezuglov.m88@gmail.com>
2024-02-21 00:42:49 +0500

    fix: error correction

--- src/test/java/ru/epa/epabackend/EpaBackendApplicationTests.java
+++
@@ -1,8 +0,0 @@
-package ru.epa.epabackend;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class EpaBackendApplicationTests {
-}
