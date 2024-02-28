552191f24a50de5d530dfbb1e6ddce535a8b53aa
Valentina <valentina.buddha@yandex.ru>
2024-02-29 00:52:27 +0300

    feature: swagger

---
+++ src/main/java/ru/epa/epabackend/config/OpenApiConfig.java
@@ -0,0 +1,13 @@
+package ru.epa.epabackend.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
+@OpenAPIDefinition(
+        info = @Info(
+                title = "\\\"Employee Performance Assessment\\\" API сервер",
+                description = "Documentation \\\"Employee Performance Assessment\\\" API v1.0", version = "1.0.0"
+        )
+)
+public class OpenApiConfig {
+}
