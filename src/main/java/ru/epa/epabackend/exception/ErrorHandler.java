a8c6b93a72a287989b72db8c6bc2b0dff9768d88
Vladislav <vladshelkovo@yandex.ru>
2024-02-28 10:20:51 +0400

    fix: edit ErrorHandler

--- src/main/java/ru/epa/epabackend/exception/ErrorHandler.java
+++ src/main/java/ru/epa/epabackend/exception/ErrorHandler.java
@@ -1,6 +1,8 @@
 package ru.epa.epabackend.exception;
 
+import jakarta.persistence.EntityNotFoundException;
 import jakarta.validation.ConstraintViolationException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -9,53 +11,44 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import ru.epa.epabackend.exception.exceptions.WrongFullNameException;
-import ru.epa.epabackend.util.Constants;
 
-import java.security.InvalidParameterException;
-import java.time.LocalDateTime;
-
+@Slf4j
 @RestControllerAdvice
 public class ErrorHandler {
+    @ExceptionHandler({MethodArgumentNotValidException.class, ConstraintViolationException.class,
+            WrongFullNameException.class, MissingServletRequestParameterException.class,
+            BadRequestException.class})
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse validateException(RuntimeException e) {
+        log.info(e.getMessage());
+        return new ErrorResponse(HttpStatus.BAD_REQUEST, e.getMessage());
+    }
+
     @ExceptionHandler(NotFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
-    public ErrorResponse handleNotFoundException(final RuntimeException e) {
-        return new ErrorResponse(e.getMessage());
+    public ErrorResponse entityNotFoundException(EntityNotFoundException e) {
+        log.info(e.getMessage());
+        return new ErrorResponse(HttpStatus.NOT_FOUND, e.getMessage());
     }
 
-    @ExceptionHandler({
-            DataIntegrityViolationException.class,
-            MethodArgumentNotValidException.class,
-            InvalidParameterException.class,
-            MissingServletRequestParameterException.class,
-            ConstraintViolationException.class,
-            WrongFullNameException.class,
-            MethodArgumentNotValidException.class, BadRequestException.class
-    })
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ErrorMessage handleBadRequest(final Exception e) {
-        return new ErrorMessage(
-                e.getClass().getSimpleName(),
-                e.getMessage(),
-                "",
-                "BAD_REQUEST",
-                Constants.DATE_TIME_SPACE.format(LocalDateTime.now()));
-    }
-
-    @ExceptionHandler({ConflictException.class})
+    @ExceptionHandler({DataIntegrityViolationException.class, ConflictException.class})
     @ResponseStatus(HttpStatus.CONFLICT)
-    public ErrorResponse handleDoubleData(final Exception e) {
-        return new ErrorResponse(e.getMessage());
+    public ErrorResponse conflictException(RuntimeException e) {
+        log.info(e.getMessage());
+        return new ErrorResponse(HttpStatus.CONFLICT, e.getMessage());
     }
 
     @ExceptionHandler({AccessDeniedException.class})
     @ResponseStatus(HttpStatus.FORBIDDEN)
     public ErrorResponse handleForbidden(final Exception e) {
-        return new ErrorResponse(e.getMessage());
+        return new ErrorResponse(HttpStatus.FORBIDDEN, e.getMessage());
     }
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public ErrorResponse handleThrowable(final Throwable e) {
-        return new ErrorResponse(e.getMessage());
+        log.error("Error thrown: {}", e.getClass());
+        log.error("Error message: {}", e.getMessage());
+        return new ErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage());
     }
 }
