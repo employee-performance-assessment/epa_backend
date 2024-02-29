5b7ceb85ce0cc67c49808405434ca6881256d831
hellotema90 <as.masalkin@yandex.ru>
2024-02-29 09:21:38 +0300

    refactor: refactor problems and add logs

--- src/main/java/ru/epa/epabackend/repository/TechnologyRepository.java
+++ src/main/java/ru/epa/epabackend/repository/TechnologyRepository.java
@@ -4,5 +4,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import ru.epa.epabackend.model.Technology;
 
 public interface TechnologyRepository extends JpaRepository<Technology, Long> {
-    boolean existTechnologyById(Long id);
 }
