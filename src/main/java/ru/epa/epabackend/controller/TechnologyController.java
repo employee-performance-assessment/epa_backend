a79d1f565921ebd0095e6513a11a5b5dda113811
hellotema90 <as.masalkin@yandex.ru>
2024-03-01 23:48:31 +0300

    refactor: refactor problems and add logs

--- src/main/java/ru/epa/epabackend/controller/TechnologyController.java
+++ src/main/java/ru/epa/epabackend/controller/TechnologyController.java
@@ -1,7 +1,6 @@
 package ru.epa.epabackend.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.validation.annotation.Validated;
