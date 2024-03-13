9767c7c3ea1646ec39dc098ddcdee5d857bc41a2
hellotema90 <as.masalkin@yandex.ru>
2024-03-14 00:38:55 +0300

    refactor: LogbookConfiguration

--- src/main/java/ru/epa/epabackend/config/LogbookConfiguration.java
+++ src/main/java/ru/epa/epabackend/config/LogbookConfiguration.java
@@ -2,20 +2,37 @@ package ru.epa.epabackend.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.zalando.logbook.Logbook;
+import org.zalando.logbook.BodyFilter;
+import org.zalando.logbook.HeaderFilter;
+import org.zalando.logbook.core.HeaderFilters;
 
-import static org.zalando.logbook.core.HeaderFilters.*;
-import static org.zalando.logbook.json.JsonPathBodyFilters.jsonPath;
+import java.util.Set;
+
+import static org.zalando.logbook.BodyFilter.merge;
+import static org.zalando.logbook.core.BodyFilters.defaultValue;
+import static org.zalando.logbook.json.JsonBodyFilters.replaceJsonStringProperty;
 
 @Configuration
 public class LogbookConfiguration {
     @Bean
-    public Logbook logbook() {
-        Logbook logbook = Logbook.builder()
-                .bodyFilter(jsonPath("$.password").replace("{masked}"))
-                .bodyFilter(jsonPath("$.token").replace("{masked}"))
-                .headerFilter(authorization())
-                .build();
-        return logbook;
+    public BodyFilter bodyFilter() {
+        Set<String> properties = Set.of
+                (
+                        "password",
+                        "token"
+                );
+
+        return merge(
+                defaultValue(),
+                replaceJsonStringProperty(properties, "{masked}"));
+    }
+
+    @Bean
+    public HeaderFilter headerFilter() {
+        Set<String> headers = Set.of(
+                "Authorization"
+        );
+
+        return HeaderFilters.replaceHeaders(headers, "{masked}");
     }
 }
