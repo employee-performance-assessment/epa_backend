7602e9f040a1c4b5cd9dc7d4cc77f27b7ac452e8
Egor Yakovlev <riddik24@yandex.ru>
2024-03-15 11:05:31 +0300

    checkstyle update

--- src/main/java/ru/epa/epabackend/controller/user/EmployeeControllerUser.java
+++ src/main/java/ru/epa/epabackend/controller/user/EmployeeControllerUser.java
@@ -5,7 +5,6 @@ import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import ru.epa.epabackend.dto.employee.EmployeeFullResponseDto;
