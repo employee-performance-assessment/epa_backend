f6d57676b9b42a79da9bccb6dbd56bc1030e4e42
Vladislav <vladshelkovo@yandex.ru>
2024-03-20 12:25:47 +0300

    fix after review

--- src/main/java/ru/epa/epabackend/util/StringPatterns.java
+++ src/main/java/ru/epa/epabackend/util/StringPatterns.java
@@ -4,6 +4,6 @@ public class StringPatterns {
 
     public static final String CYRILIC_LATIN_ALPHABET_AND_NUMBERS = "^[a-zA-Zа-яА-ЯЁё0-9\\s\\-\\.\\,]+$";
 
-    public StringPatterns() {
+    private StringPatterns() {
     }
 }
