0ec8c7b7e63778139816af8aa81599aa45e03082
Valentina Vakhlamova <valentina.buddha@yandex.ru>
2024-03-31 19:33:53 +0300

    fix: валидация

--- src/main/java/ru/epa/epabackend/dto/employee/RequestJwt.java
+++ src/main/java/ru/epa/epabackend/dto/employee/RequestJwt.java
@@ -6,18 +6,21 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import static ru.epa.epabackend.util.StringPatterns.LATIN_NUMBERS_SPECIAL_CHARACTERS_AND_ONE_UPPERCASE_LETTER;
+
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 public class RequestJwt {
+
     @NotBlank
     @Email
     @Size(min = 5, max = 50)
     private String email;
 
     @NotEmpty
-    @Pattern(regexp = "^[a-zA-Z0-9.,:;?!*+%\\-<>@\\[\\]\\{\\}\\/\\\\\\_\\$\\#]+$")
+    @Pattern(regexp = LATIN_NUMBERS_SPECIAL_CHARACTERS_AND_ONE_UPPERCASE_LETTER)
     @Size(min = 8, max = 14)
     private String password;
 
