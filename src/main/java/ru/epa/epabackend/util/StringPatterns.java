9f7bb5553dda1c559df13678e114ed1febe10788
Valentina Vakhlamova <valentina.buddha@yandex.ru>
2024-04-03 21:19:05 +0300

    fix: criteriy validation

--- src/main/java/ru/epa/epabackend/util/StringPatterns.java
+++ src/main/java/ru/epa/epabackend/util/StringPatterns.java
@@ -6,8 +6,9 @@ public class StringPatterns {
     public static final String CYRILLIC_LATIN_WHITESPACE_AND_DASH = "^[а-яА-ЯЁёa-zA-Z\\s\\-]+$";
     public static final String LATIN_NUMBERS_SPECIAL_CHARACTERS_AND_ONE_UPPERCASE_LETTER =
             "^[a-zA-Z0-9\\.\\,\\:\\;\\?\\!\\*\\+\\%\\-\\<\\>\\@\\[\\]\\{\\}\\/\\\\\\_\\$\\#]+$";
+    public static final String CYRILLIC_LATIN_NUMBERS_SPECIAL_CHARACTERS =
+            "^[a-zA-Zа-яА-ЯЁё0-9\\.\\,\\:\\;\\?\\!\\*\\+\\%\\-\\<\\>\\@\\[\\]\\{\\}\\/\\\\\\_\\$\\#]+$";
     public static final String TELEGRAM = "^@{1}[a-zA-Z0-9\\_]+$";
-
 
     private StringPatterns() {
     }
