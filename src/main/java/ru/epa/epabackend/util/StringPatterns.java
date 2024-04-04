4bca5b8c4e196ca487ca5787ad8d446ca44ca9bd
Valentina Vakhlamova <valentina.buddha@yandex.ru>
2024-04-04 10:02:44 +0300

    fix: validation pattern

--- src/main/java/ru/epa/epabackend/util/StringPatterns.java
+++ src/main/java/ru/epa/epabackend/util/StringPatterns.java
@@ -3,11 +3,14 @@ package ru.epa.epabackend.util;
 public class StringPatterns {
 
     public static final String CYRILLIC_LATIN_ALPHABET_AND_NUMBERS = "^[a-zA-Zа-яА-ЯЁё0-9\\s\\-\\.\\,]+$";
+
     public static final String CYRILLIC_LATIN_WHITESPACE_AND_DASH = "^[а-яА-ЯЁёa-zA-Z\\s\\-]+$";
+
     public static final String LATIN_NUMBERS_SPECIAL_CHARACTERS_AND_ONE_UPPERCASE_LETTER =
-            "^[a-zA-Z0-9\\.\\,\\:\\;\\?\\!\\*\\+\\%\\-\\<\\>\\@\\[\\]\\{\\}\\/\\\\\\_\\$\\#]+$";
+            "^[a-zA-Z0-9.,:;?!*+%\\-<>@\\[\\]{}/_$#]+$";
+
     public static final String CYRILLIC_LATIN_NUMBERS_SPECIAL_CHARACTERS =
-            "^[a-zA-Zа-яА-ЯЁё0-9.,:;?!*+%-<>@[\\]{}/\\_$#]+$";
+            "^[a-zA-Zа-яА-ЯЁё0-9.,:;?!*+%\\-<>@\\[\\]{}/_$#]+$";
 
     public static final String TELEGRAM = "^@{1}[a-zA-Z0-9\\_]+$";
 
