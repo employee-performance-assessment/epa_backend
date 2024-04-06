dc7ccea06a0f5a628b6d2e22fefaefcd05188585
Константин <zed726804@yandex.ru>
2024-04-06 12:00:11 +0300

    fix(default-criterias): restore endpoint

---
+++ src/main/java/ru/epa/epabackend/controller/admin/AdminCriteriaController.java
@@ -0,0 +1,59 @@
+package ru.epa.epabackend.controller.admin;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import ru.epa.epabackend.dto.criteria.ResponseCriteriaDto;
+import ru.epa.epabackend.exception.ErrorResponse;
+import ru.epa.epabackend.mapper.CriteriaMapper;
+import ru.epa.epabackend.model.Criteria;
+import ru.epa.epabackend.service.CriteriaService;
+
+import java.util.List;
+
+/**
+ * Класс AdminCriteriaController содержит эндпойнты для администратора, относящиеся к критериям оценок.
+ *
+ * @author Михаил Безуглов
+ */
+@Tag(name = "Admin: Критерии оценок", description = "API администратора для работы с критериями оценок")
+@SecurityRequirement(name = "JWT")
+@Validated
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/admin/criteria")
+public class AdminCriteriaController {
+
+    private final CriteriaService criteriaService;
+    private final CriteriaMapper criteriaMapper;
+
+    /**
+     * Эндпойнт получения дефолтных критериев (по умолчанию).
+     */
+    @Operation(summary = "Эндпойнт получения дефолтных критериев (по умолчанию).")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "OK", content = @Content(
+                    mediaType = "application/json", array = @ArraySchema(
+                    schema = @Schema(implementation = ResponseCriteriaDto.class)))),
+            @ApiResponse(responseCode = "400", description = "BAD_REQUEST", content = @Content(
+                    mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class))),
+            @ApiResponse(responseCode = "401", description = "UNAUTHORIZED", content = @Content(
+                    mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class))),
+            @ApiResponse(responseCode = "403", description = "FORBIDDEN", content = @Content(
+                    mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class)))})
+    @GetMapping("/default")
+    public List<ResponseCriteriaDto> findDefault() {
+        List<Criteria> criterias = criteriaService.findDefault();
+        return criteriaMapper.mapList(criterias);
+    }
+}
