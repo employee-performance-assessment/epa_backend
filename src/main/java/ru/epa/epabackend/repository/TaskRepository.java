c971b5dc87f8293439ff3f63d6383cc1572ddff9
Константин <zed726804@yandex.ru>
2024-04-24 03:17:37 +0300

    fix(tasks): checkstyle

--- src/main/java/ru/epa/epabackend/repository/TaskRepository.java
+++ src/main/java/ru/epa/epabackend/repository/TaskRepository.java
@@ -2,7 +2,6 @@ package ru.epa.epabackend.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import ru.epa.epabackend.model.Task;
 import ru.epa.epabackend.util.TaskStatus;
 
