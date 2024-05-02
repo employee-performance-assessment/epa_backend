e14b82c505efe2540adc6848aff8bf5e4a70c231
Valentina Vakhlamova <valentina.buddha@yandex.ru>
2024-05-02 17:38:57 +0300

    remove courses and technologies

--- src/main/java/ru/epa/epabackend/repository/TechnologyRepository.java
+++
@@ -1,7 +0,0 @@
-package ru.epa.epabackend.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import ru.epa.epabackend.model.Technology;
-
-public interface TechnologyRepository extends JpaRepository<Technology, Long> {
-}
